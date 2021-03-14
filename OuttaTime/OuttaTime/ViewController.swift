3b2004da109e361c2215373fd7ae720e982876b9
Marc <marc.daniel.jacques@gmail.com>
2021-03-13 20:32:04 -0500

    - completed storyboard design and auto layout constraints are in place. - organized folders by roles - created time circuit and date picker view controller

---
+++ OuttaTime/OuttaTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OuttaTime
+//
+//  Created by Marc Jacques on 3/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
